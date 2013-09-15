5b580f605de62506e56c55fa2ac18b3e388c55fe
kodeartist <kodeartist@4c9e993c-7777-c7b4-a5ab-8c2a4b837bd7>
2013-09-15 10:45:01 +0000

    Version 1.0  First commit to SVN

---
+++ trunk/Syslog Server/src/com/home/server/ServerMain.java
@@ -0,0 +1,30 @@
+package com.home.server;
+
+import org.productivity.java.syslog4j.server.SyslogServer;
+import org.productivity.java.syslog4j.server.SyslogServerEventHandlerIF;
+import org.productivity.java.syslog4j.server.SyslogServerIF;
+import org.productivity.java.syslog4j.server.impl.net.udp.UDPNetSyslogServerConfig;
+
+
+public class ServerMain {
+	int port = 514;
+	String host ;
+ServerMain(String host, int port)
+{
+	this.port = port;
+	this.host = host;
+}
+
+void serverStart()
+{
+	UDPNetSyslogServerConfig udpConfig = new UDPNetSyslogServerConfig(host,port);
+	SyslogServerEventHandlerIF()
+    udpConfig.addEventHandler(new );
+    udpConfig.setUseDaemonThread(false);
+
+    SyslogServerIF server = SyslogServer.createInstance(host + port, udpConfig);
+    server.run();
+	
+}
+
+}
---
+++ trunk/Syslog Server/src/com/home/server/SyslogMain.java
@@ -0,0 +1,13 @@
+package com.home.server;
+
+
+
+public class SyslogMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  
+
+	}
+
+}
